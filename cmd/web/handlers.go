3f7a70af396c25402f7129fa73c26ab2b09cfdbd
calebsenm <calebsenm@gmail.com>
2024-08-19 10:15:41 -0500

    add styles and serve static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,23 +13,33 @@ func home(w http.ResponseWriter, r *http.Request ){
         http.NotFound(w , r );
         return 
     }
-    
-    ts , err := template.ParseFiles("./ui/html/pages/home.tmpl");
-    
-    if err != nil {
-        log.Print(err.Error());
-        http.Error(w, "Internal Server Error", 500)
-         return
-
+    // Initialize a slice containing the paths to the two files. It's important
+    // to note that the file containing our base template must be the *first*
+    // file in the slice.
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/pages/home.tmpl",
+        "./ui/html/partials/nav.tmpl",
     }
 
-    err = ts.Execute(w , nil )
-
+    // Use the template.ParseFiles() function to read the files and store the
+    // templates in a template set. Notice that we can pass the slice of file
+    // paths as a variadic parameter?
+    ts , err := template.ParseFiles(files...);
+    if err != nil {
+        log.Print(err.Error());
+        http.Error(w , "Internal Server Error", 500);
+        return 
+    }
+    
+    // Use the ExecuteTemplate() method to write the content of the "base" 
+    // template as the response body.
+    
+    err = ts.ExecuteTemplate(w , "base", nil )
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal Server Error", 500)
     }
-    
 }
 
 func snippetView(w http.ResponseWriter , r *http.Request ){
