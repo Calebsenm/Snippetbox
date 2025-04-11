f11128ec2ded659421c06f8170ed32487138dcb5
calebsenm <calebsenm@gmail.com>
2025-04-10 20:34:07 -0500

    embedded and generic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -76,22 +76,28 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	var form snippetCreateForm
-	err := app.decodePostForm(r, &form)
+	
+    var form snippetCreateForm
+	
+    err := app.decodePostForm(r, &form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
+
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
-	if !form.Valid() {
+	
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+    if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
 		return
 	}
+
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,8 +4,9 @@ import (
 	"html/template"
 	"path/filepath"
 	"time"
-
+    "io/fs"
 	"github.com/calebsenm/snippetbox/internal/models"
+    "github.com/calebsenm/snippetbox/ui"
 )
 
 type templateData struct {
@@ -32,39 +33,30 @@ var functions = template.FuncMap{
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	
+    cache := map[string]*template.Template{}
+    pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 
 	if err != nil {
 		return nil, err
 	}
 
 	for _, page := range pages {
-		name := filepath.Base(page)
+        name := filepath.Base(page)
 
-		// The template.FuncMap must be registered with the template set before you
-		// call the ParseFiles() method. This means we have to use template.New() to
-		// create an empty template set, use the Funcs() method to register the
-		// template.FuncMap, and then parse the file as normal.
+        patterns := []string{
+            "html/base.tmpl",
+            "html/partials/*.tmpl",
+            page, 
+        }
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+        ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
+        if err != nil {
+            return nil, err
+        }
 
-		if err != nil {
-			return nil, err
-		}
+        cache[name] = ts
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-
-		if err != nil {
-			return nil, err
-		}
-
-		cache[name] = ts
-	}
+    }
 	return cache, nil
 }
